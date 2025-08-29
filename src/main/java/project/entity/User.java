ee163ffdfb551348f70f18d86a4c6d5e9401e5fe
NguyenSinh1902 <lenguyensinh1902@gmail.com>
2025-08-29 09:35:09 +0700

    Add entity/User.java

---
+++ src/main/java/project/entity/User.java
@@ -0,0 +1,56 @@
+package project.entity;
+
+public class User {
+    private int userId;
+    private String username;
+    private String passwordHash;
+    private String role;
+
+    public User() {
+    }
+
+    public User(int userId, String username, String passwordHash, String role) {
+        this.userId = userId;
+        this.username = username;
+        this.passwordHash = passwordHash;
+        this.role = role;
+    }
+
+    // Getter & Setter
+    public int getUserId() {
+        return userId;
+    }
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public String getRole() {
+        return role;
+    }
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + userId +
+                ", username='" + username + '\'' +
+                ", role='" + role + '\'' +
+                '}';
+    }
+}
