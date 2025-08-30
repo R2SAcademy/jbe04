57e7d587e1375319e33905bb7a2847e3bb202706
HuyNguyen1112 <volibear918@gmail.com>
2025-08-30 19:42:01 +0700

    Revert "feat: Product"
    This reverts commit b5029e646ab7a7442e2ae2aadd5c8be63857010c.

--- src/main/java/project/util/Constants.java
+++ src/main/java/project/util/Constants.java
@@ -3,14 +3,4 @@ package project.util;
 public class Constants {
     public static final String BRAND_HEADER = String.format("%-10s | %-30s", "Brand ID", "Brand Name");
     public static final String BRAND_ROW_FORMAT = "%-10d | %-30s";
-    public static final String PRODUCT_ID = "product_id";
-    public static final String PRODUCT_NAME = "name";
-    public static final String CATEGORY_ID = "category_id";
-    public static final String BRAND_ID = "brand_id";
-    public static final String MODEL_YEAR = "model_year";
-    public static final String PRICE = "price";
-    public static final String DESCRIPTION = "description";
-    public static final String STOCK_ID = "stock_id";
-    public static final String STORE_ID = "store_id";
-    public static final String QUANTITY = "quantity";
 }
--- src/main/java/project/util/ValidationUtil.java
+++ src/main/java/project/util/ValidationUtil.java
@@ -4,20 +4,4 @@ public class ValidationUtil {
     public static boolean isValidString(String input) {
         return input != null && !input.trim().isEmpty();
     }
-
-    public static boolean isValidId(int input) {
-        return input != 0;
-    }
-
-    public static boolean isValidPrice(Float input) {
-        if (input == null)
-            return true;
-        else return input > 0;
-    }
-
-    public static boolean isValidYear(Integer input) {
-        if (input == null)
-            return true;
-        else return input >= 1900 && input <= 2100;
-    }
 }
